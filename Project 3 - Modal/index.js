c5e30c64a2f78340df0a847999543d1024ff96cd
Thomas Sankara <thomassankara74@gmail.com>
2022-06-09 12:21:40 +0300

    script file

---
+++ Project 3 - Modal/index.js
@@ -0,0 +1,26 @@
+//
+// METHOD 1
+// const openModal = document.getElementById("open-modal")
+// const closeModal = document.getElementById("close-modal")
+// const modal = document.querySelector(".modal")
+
+// openModal.addEventListener("click", () => {
+//   modal.classList.add("modal-open")
+// })
+
+// closeModal.addEventListener("click", () => {
+//   modal.classList.remove("modal-open")
+// })
+
+// METHOD 2
+// const openModal = document.getElementById("open-modal")
+// const closeModal = document.getElementById("close-modal")
+// const modal = document.querySelector("dialog")
+
+// openModal.addEventListener("click", () => {
+//   modal.showModal()
+// })
+
+// closeModal.addEventListener("click", () => {
+//   modal.close()
+// })
